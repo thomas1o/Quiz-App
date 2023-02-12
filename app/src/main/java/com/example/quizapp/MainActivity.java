ad619df94b567d83fd72b3c72c94b55734efba29
Thomas BIju <thomasbiju43210@gmail.com>
2023-02-13 01:07:38 +0530

    v1.1 Developers page

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.quizapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button start_button, about_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        start_button = (Button)findViewById(R.id.start_button);
+        about_button = (Button)findViewById(R.id.about_button);
+
+        //to change page into about page
+        about_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(MainActivity.this, "Quiz has started", Toast.LENGTH_SHORT).show();
+                setContentView(R.layout.about_page);
+            }
+        });
+
+
+
     }
 }
